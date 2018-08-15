db969dd5b0327a616da7d5d957dd24a6d2c77130
Omid <omid3098@gmail.com>
2018-08-15 13:01:40 +0430

    Inject method in inject behaviour

--- OpenFramework/core/InjectBehaviour.cs
+++ OpenFramework/core/InjectBehaviour.cs
@@ -5,12 +5,22 @@ namespace OpenFramework
     public class InjectBehaviour : MonoBehaviour
     {
         private static GameContext context;
+        private bool injected = false;
         private void Awake()
         {
+            Inject();
+        }
+        public void Inject()
+        {
+            if (injected) return;
+            injected = true;
             if (context == null) context = FindObjectOfType<GameContext>();
             foreach (var item in GetComponents<MonoBehaviour>())
                 if (item != this)
+                {
+                    Debug.Log("Injecting for " + gameObject.name);
                     context.Inject(item);
+                }
         }
     }
 }
